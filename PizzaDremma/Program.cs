6bf265f3fb50e46fc269a143a22d862c09812906
Dries Maes <dm.inbox@outlook.com>
2020-12-17 13:21:05 +0100

    Add project files.

---
+++ PizzaDremma/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PizzaDremma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
